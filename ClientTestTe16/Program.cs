795b615d7976b9c383f0e54458fc6b76d03f2335
Tobias Lindberg (Dell) <tobias.lindberg@sodertalje.ntig.se>
2019-01-08 13:55:51 +0100

    En första klient med felhantering. 11.2.1 ovh 11.2.4

--- ClientTestTe16/Program.cs
+++ ClientTestTe16/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,37 @@ namespace ClientTestTe16
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string address = "127.0.0.1";
+                int port = 8001;
+
+                //Anslut till server
+                Console.WriteLine("Ansluter...");
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(address, port);
+                Console.WriteLine("Ansluten!");
+
+                //Skriv meddelande att skicka
+                Console.Write("Skriv meddelande: ");
+                string message = Console.ReadLine();
+
+                //Konvertera meddelande till UTF8-bytes
+                Byte[] bMessage =
+                    System.Text.Encoding.UTF8.GetBytes(message);
+
+                //Skicka iväg meddelandet
+                Console.WriteLine("Skickar...");
+                NetworkStream tcpStream = tcpClient.GetStream();
+                tcpStream.Write(bMessage, 0, bMessage.Length);
+                tcpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
